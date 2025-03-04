8324158a8fb193059ffbda65cbb06472e14f9717
mateja503 <matejanikolikj503@gmail.com>
2025-03-04 10:34:00 +0100

    Finished angular crash course

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,11 +1,14 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { UiService } from '../../services/ui.service';
 import { Task } from '../../Tasks';
+import { Subscription } from 'rxjs';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
@@ -17,7 +20,12 @@ export class AddTaskComponent {
   text!: string;
   day!: string;
   reminder: boolean = false;
+  showAddTask!: boolean
+  subscription: Subscription
 
+  constructor (private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value)=> this.showAddTask = value)
+  }
 
 
   onSubmit() {
